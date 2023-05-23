69494253ab4968370b3e9a02dde9865822b9b20d
Dorian Bweta <dorianbweta@gmail.com>
2023-05-23 15:35:40 +0800

    task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    # I create an instance variable that I can re-use in my views!
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # No need for app/views/tasks/create.html.erb
+    redirect_to task_path(@task[:id])
+  end
+
+  def edit; end
+
+  def update
+    @task.update(task_params)
+    # No need for app/views/tasks/update.html.erb
+    redirect_to task_path(@task[:id])
+  end
+
+  def destroy
+    @task.destroy
+    # No need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
